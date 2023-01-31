4cea6407015074b122223d7ee4fdc03538ee341b
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-01-31 19:13:20 +0100

    feat: make environment file with api urls

---
+++ ng-pokemon-trainer/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUsers: "https://observant-seed-monday.glitch.me/trainers",
+    apiPokemons: "https://pokeapi.co/api/v2/pokemon",
+    apiPokemonImages:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon"
+}
