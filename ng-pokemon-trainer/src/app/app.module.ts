4d3ea28c53c203cfb83d9bffc7391beceb9617a3
HvardLund <122644940+HvardLund@users.noreply.github.com>
2023-02-01 10:03:33 +0100

    Update app.module.ts

--- ng-pokemon-trainer/src/app/app.module.ts
+++ ng-pokemon-trainer/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { FormsModule } from '@angular/forms';
     TrainerPage,
     CataloguePage,
     Pokemon,
-    PokemonListComponent
+    PokemonListComponent,
     LoginFormComponent
   ],
   imports: [// modules
