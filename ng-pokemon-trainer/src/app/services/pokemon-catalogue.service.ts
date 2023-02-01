f37efc5cd466529d1f444745a228c9b794222923
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-02-01 14:08:20 +0100

    chore, wip: refactoring

--- ng-pokemon-trainer/src/app/services/pokemon-catalogue.service.ts
+++ ng-pokemon-trainer/src/app/services/pokemon-catalogue.service.ts
@@ -1,47 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { finalize } from 'rxjs';
-import { Pokemon } from 'src/app/models/pokemon.model'
+import { Pokemon } from 'src/app/models/pokemon.model';
 import { PokemonResponse } from 'src/app/models/PokemonResponse';
 import { environment } from 'src/environments/environment';
 
-const { apiPokemons, apiPokemonImages } = environment // = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
+const { apiPokemons, apiPokemonImages } = environment; // = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonCatalogueService {
-
-  private _pokemon_list:Pokemon[] = []
-  private _error: string = ""
+  private _pokemon_list: Pokemon[] = [];
+  private _error: string = '';
   private _loading: boolean = false;
 
   get pokemon(): Pokemon[] {
-
-    return this._pokemon_list
+    return this._pokemon_list;
   }
 
   get error(): string {
-    return this._error
+    return this._error;
   }
 
   get loading(): boolean {
-    return this._loading
+    return this._loading;
   }
 
   constructor(private readonly http: HttpClient) {}
-  
-  public findPokemonAndTheirImage(limit:number, offset:number): void {
+
+  public findPokemonAndTheirImage(limit: number, offset: number): void {
     this._loading = true;
-    this.http.get<PokemonResponse>(apiPokemons + `?limit=${limit}&offset=${offset}`).pipe(finalize(() => {this._loading = false})).subscribe({
-      next: (pokemon_list:PokemonResponse) => {
-        this._pokemon_list = pokemon_list.results;
-        this._pokemon_list.forEach(pokemon => pokemon.img=
-          `${apiPokemonImages + (pokemon.url.split('/').slice(-2, -1).pop())}.png`)
-      },
-      error: (error: HttpErrorResponse) => {
-        this._error = error.message
-      }
-    })
+    this.http
+      .get<PokemonResponse>(apiPokemons + `?limit=${limit}&offset=${offset}`)
+      .pipe(
+        finalize(() => {
+          this._loading = false;
+        })
+      )
+      .subscribe({
+        next: (pokemon_list: PokemonResponse) => {
+          this._pokemon_list = pokemon_list.results;
+
+          // fetch image
+          this._pokemon_list.forEach((pokemon) => {
+            const id = pokemon.url.split('/').slice(-2, -1).pop();
+            pokemon.img = `${apiPokemonImages + id}.png`;
+          });
+        },
+        error: (error: HttpErrorResponse) => {
+          this._error = error.message;
+        },
+      });
   }
 }
