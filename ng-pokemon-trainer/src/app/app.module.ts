ec8f42a48158b97567f35f04e03efb23a5ced4ed
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-02-01 10:03:38 +0100

    chore: tidy imports

--- ng-pokemon-trainer/src/app/app.module.ts
+++ ng-pokemon-trainer/src/app/app.module.ts
@@ -1,17 +1,18 @@
+//modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
+// components
 import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
 import { TrainerPage } from './pages/trainer/trainer.page';
 import { CataloguePage } from './pages/catalogue/catalogue.page';
-import { AppRoutingModule } from './app-routing.module';
 import { Pokemon } from './components/pokemon/pokemon.component';
-import { HttpClientModule } from "@angular/common/http";
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({ //components
   declarations: [
@@ -20,7 +21,7 @@ import { FormsModule } from '@angular/forms';
     TrainerPage,
     CataloguePage,
     Pokemon,
-    PokemonListComponent
+    PokemonListComponent,
     LoginFormComponent
   ],
   imports: [// modules
