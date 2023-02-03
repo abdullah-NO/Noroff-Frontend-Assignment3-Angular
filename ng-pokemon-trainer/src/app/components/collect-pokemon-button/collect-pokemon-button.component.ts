4b8fa7ffbec6d279fd6c332ee895b2b98b036695
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-02-03 07:35:49 +0100

    chore: Rename service

--- ng-pokemon-trainer/src/app/components/collect-pokemon-button/collect-pokemon-button.component.ts
+++ ng-pokemon-trainer/src/app/components/collect-pokemon-button/collect-pokemon-button.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { Trainer } from 'src/app/models/trainer.model';
-import { TrainerApiService } from 'src/app/services/trainer-api.service';
+import { PokemonCollectionService } from 'src/app/services/pokemon-collection.service';
 
 @Component({
   selector: 'app-collect-pokemon-button',
@@ -12,16 +12,18 @@ export class CollectPokemonButtonComponent {
   @Input() pokemonName: string = '';
 
   get loading(): boolean {
-    return this.trainerApiService.loading;
+    return this.pokemonCollectionService.loading;
   }
 
-  constructor(private readonly trainerApiService: TrainerApiService) {}
+  constructor(
+    private readonly pokemonCollectionService: PokemonCollectionService
+  ) {}
 
   onCollectClick(): void {
     // Disable button
 
     // Add pokemon to Trainer API and to sessionStorage
-    this.trainerApiService.addPokemon(this.pokemonName).subscribe({
+    this.pokemonCollectionService.addPokemon(this.pokemonName).subscribe({
       next: (trainer: Trainer) => {},
       error: (error: HttpErrorResponse) => {
         console.error('Error: ', error.message);
